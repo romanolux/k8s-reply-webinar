4f6cb0013c3dcf076b8ffc555ec31f6e71206708
r.lucernati <r.lucernati@reply.it>
2020-03-29 20:34:51 +0200

    working version

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/romanolux/k8s-reply-webinar
 go 1.14
 
 require (
+	github.com/aws/aws-sdk-go v1.29.34
 	github.com/imdario/mergo v0.3.9 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	k8s.io/api v0.18.0 // indirect
+	k8s.io/api v0.18.0
 	k8s.io/apimachinery v0.18.0
 	k8s.io/client-go v0.0.0-20200323042902-70eb4849511b
-        k8s.io/utils v0.0.0-20200327001022-6496210b90e8 // indirect
+	k8s.io/utils v0.0.0-20200327001022-6496210b90e8 // indirect
 )
--- handler.go
+++ handler.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	stormv1 "github.com/romanolux/k8s-reply-webinar/pkg/apis/k8dynamo/v1"
 )
 
 // Handler interface contains the methods that are required
@@ -23,7 +29,17 @@ func (t *TestHandler) Init() error {
 
 // ObjectCreated is called when an object is created
 func (t *TestHandler) ObjectCreated(obj interface{}) {
-	fmt.Println("TestHandler.ObjectCreated")
+	k8sdynamo, _ := obj.(*stormv1.K8dynamo)
+
+	tableName := k8sdynamo.Spec.TableName
+	readCapacity := int64(*k8sdynamo.Spec.ReadUnit)
+	writeCapacity := int64(*k8sdynamo.Spec.WriteUnit)
+	// fmt.Printf("%s %d %d", tableName, readCapacity, writeCapacity)
+	fmt.Println("Start Object Creation")
+	fmt.Printf("Instance Name: %s /n", k8sdynamo.Name)
+	fmt.Printf("Instance Values: %s %d %d /n", tableName, readCapacity, writeCapacity)
+	callAWSClient(tableName, readCapacity, writeCapacity)
+	// fmt.Println("Object Created")
 }
 
 // ObjectDeleted is called when an object is deleted
@@ -34,4 +50,52 @@ func (t *TestHandler) ObjectDeleted(obj interface{}) {
 // ObjectUpdated is called when an object is updated
 func (t *TestHandler) ObjectUpdated(objOld, objNew interface{}) {
 	fmt.Println("TestHandler.ObjectUpdated")
+}
+
+func callAWSClient(tableName string, read, write int64) {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("eu-west-1"),
+		Credentials: credentials.NewSharedCredentials("", "saml"),
+	})
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("Name"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("Value"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("Name"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("Value"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(read),
+			WriteCapacityUnits: aws.Int64(write),
+		},
+		TableName: aws.String(tableName),
+	}
+
+	res, err := svc.CreateTable(input)
+	if err != nil {
+		fmt.Println("Got error calling CreateTable:")
+		fmt.Println(err.Error())
+		// os.Exit(1)
+		return
+	}
+	fmt.Println(res)
+	fmt.Println("Created the table", tableName)
 }
